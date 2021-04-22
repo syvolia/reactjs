3ccc67b65be2693efd2d439e8728ee1361edb035
syvolia <syvoliamary@gmail.com>
2021-04-22 21:56:59 +0300

    second committ

---
+++ build/precache-manifest.d69c99c9acf19445e7cf065a7ba9e9b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7e34f49dcf745bf455bc",
+    "url": "/static/js/main.7e34f49d.chunk.js"
+  },
+  {
+    "revision": "c6a6e9ff01a47014ce91",
+    "url": "/static/js/1.c6a6e9ff.chunk.js"
+  },
+  {
+    "revision": "7e34f49dcf745bf455bc",
+    "url": "/static/css/main.7be6449d.chunk.css"
+  },
+  {
+    "revision": "f4715693b5ac1b7d172241fdc51c2c5d",
+    "url": "/index.html"
+  }
+];
